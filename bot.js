5f1e6575d1e44f08fa0db402cb0d5d1ba727ad9a
zRazbliuto <42890358+zRazbliuto@users.noreply.github.com>
2018-09-05 12:29:42 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -4,7 +4,7 @@ const prefix = '.'
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
-client.user.setGame(` #Bُubbles Tumblr,2kk .. `,"http://twitch.tv/S-F")
+client.user.setGame(` #Bُubbles Tumblr,2kk .. | #Outt. `,"http://twitch.tv/S-F")
   console.log('')
   console.log('')
   console.log('╔[═════════════════════════════════════════════════════════════════]╗')
