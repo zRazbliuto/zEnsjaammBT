fdf6895ccc5b2e6a868015279de76a1e48f24a9b
zRazbliuto <42890358+zRazbliuto@users.noreply.github.com>
2018-10-26 13:57:52 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -4,7 +4,7 @@ const prefix = '.'
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
-client.user.setGame(` #The Presidency Zila. `,"http://twitch.tv/Catraz")
+client.user.setGame(` #Catraz Venom Network 4k .. `,"http://twitch.tv/Catraz")
   console.log('')
   console.log('')
   console.log('╔[═════════════════════════════════════════════════════════════════]╗')
